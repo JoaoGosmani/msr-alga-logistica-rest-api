4bef3290d48b6cde06858517a31148519b7f1265
Joao Gosmani <joaogosmani@gmail.com>
2023-01-18 21:00:08 -0300

    Projeto criado

---
+++ src/test/java/br/com/joaogosmani/algalogistica/AlgaLogisticaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaogosmani.algalogistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgaLogisticaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
