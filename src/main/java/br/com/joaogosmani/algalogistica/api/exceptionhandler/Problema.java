7985f6515a9c7273620b9d232285cf6b5974383e
Joao Gosmani <joaogosmani@gmail.com>
2023-01-19 14:14:56 -0300

    Implementando Exception Handler

---
+++ src/main/java/br/com/joaogosmani/algalogistica/api/exceptionhandler/Problema.java
@@ -0,0 +1,35 @@
+package br.com.joaogosmani.algalogistica.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Problema {
+
+	private Integer status;
+	
+	private LocalDateTime dataHora;	
+	
+	private String titulo;
+	
+	private List<Campo> campos;
+	
+	@Getter
+	@AllArgsConstructor
+	public static class Campo {
+		
+		private String nome;
+		
+		private String mensagem;
+		
+	}
+	
+}
